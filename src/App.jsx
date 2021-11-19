26d706dc9b4ea2a15f8e2e2cc615662cdb141bd7
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-11-19 16:24:08 -0300

    created the globalStyle form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-
+import GlobalStyle from './assets/style/GlobalStyle';
+// eslint-disable-next-line func-names
 const App = function () {
   return (
     <div className="App">
+      <GlobalStyle />
       <h1>Camilo</h1>
     </div>
   );
