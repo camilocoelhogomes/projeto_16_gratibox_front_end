34fc040d708e0e03cad559b3329cffc63d720c87
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-11-20 17:32:54 -0300

    layout of sign-up implemented

---
+++ src/assets/style/Input.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+width: 100%;
+height: 64px;
+border: 1px solid #604848;
+border-radius: 10px;
+font-size: 24px;
+padding: 18px;
+font-family: 'primaryNormal';
+
+::placeholder{
+  color: #60484866;
+}
+`;
+
+export default Input;
