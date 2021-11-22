76451ab8f891cbfe4d5e903792f964c8db8a76da
camilocoelhogomes <camilo.coelho.gomes@gmail.com>
2021-11-22 14:54:50 -0300

    finish show the user delivery date

--- src/pages/planInfo/PlanInfo.js
+++ src/pages/planInfo/PlanInfo.js
@@ -5,10 +5,10 @@ import dayjs from 'dayjs';
 import PageHeader from '../../components/PageHeader';
 import UserContext from '../../Context/UserContext';
 import img from '../../assets/images/image03.jpg';
+import Button from '../../assets/style/Button';
 
 const PlanInfo = function () {
   const { userData } = useContext(UserContext);
-  console.log(userData);
   const navigate = useNavigate();
   const weekDays = {
     Segunda: 2,
@@ -21,21 +21,18 @@ const PlanInfo = function () {
     resultDate.setDate(date.getDate() + (7 + dayOfWeek - date.getDay()) % 7);
     return resultDate;
   }
-  const [
-    deliveryDates,
-    setDeliveryDates] = useState([getNextDayOfWeek(new Date(), weekDays[userData?.deliveryDate])]);
+
+  const [nextDelivery, setNextDelivery] = useState();
 
   useEffect(() => {
     if (!userData || !userData?.userPlanSignatureDate) navigate('/');
-    const newDeliveryDates = [...deliveryDates];
-    for (let i = 0; i < 2; i += 1) {
-      // eslint-disable-next-line max-len
-      newDeliveryDates.push(getNextDayOfWeek(newDeliveryDates[i].setDate(newDeliveryDates[i].getDate() + 1), weekDays[userData?.deliveryDate]));
+    if (userData?.userPlan === 'semanal') {
+      setNextDelivery(dayjs(getNextDayOfWeek(new Date(), weekDays[userData?.deliveryDate])).format('DD/MM/YYYY'));
+    } else {
+      setNextDelivery('Calculado');
     }
-    setDeliveryDates(newDeliveryDates);
   }, []);
 
-  console.log(getNextDayOfWeek(new Date(), 4));
   return (
     <StyledPlanInfo>
       <PageHeader
@@ -62,15 +59,19 @@ const PlanInfo = function () {
         </div>
         <div className="info-area">
           <h5>
-            Próximas entregas:
+            Próxima entrega:
+            {' '}
+            {nextDelivery}
           </h5>
-          {deliveryDates.map((deliveryDate) => <h5>{ dayjs(deliveryDate).format('DD/MM/YYYY')}</h5>)}
         </div>
         <div className="products-area">
           {
             userData?.productOption.map((item) => <div><h5>{item}</h5></div>)
           }
         </div>
+      </div>
+      <div className="button-area">
+        <Button>Avaliar Entregas: A implementar</Button>
       </div>
     </StyledPlanInfo>
   );
